64a98d1496c86a085fcd791129731fe794f929de
Shockk <shokku.ra@gmail.com>
2015-09-20 22:56:43 +0100

    Add empty Data.PolyMap module skeleton

---
+++ src/Data/PolyMap.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE Safe #-}
+
+{-|
+  Module      : Data.PolyMap
+  Copyright   : (c) 2015 David Farrell
+  License     : PublicDomain
+  Stability   : unstable
+  Portability : portable
+
+  Polydirectional maps.
+-}
+
+module Data.PolyMap where
