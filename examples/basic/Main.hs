d0687d2e8786d2ccc845b4f3c4298e6c29bab7f0
Shockk <shokku.ra@gmail.com>
2015-09-21 05:19:39 +0100

    Create very basic storage structure and write test

---
+++ examples/basic/Main.hs
@@ -0,0 +1,5 @@
+import Data.PolyMap 
+
+main = print polymap
+
+polymap = ["asdf"] :<->: [5] :<->: EmptyPolyMap
--- src/Data/PolyMap.hs
+++ src/Data/PolyMap.hs
@@ -1,4 +1,11 @@
 {-# LANGUAGE Safe #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 {-|
   Module      : Data.PolyMap
@@ -11,3 +18,12 @@
 -}
 
 module Data.PolyMap where
+
+data family PolyMap :: [*] -> *
+data instance PolyMap '[] = EmptyPolyMap
+data instance PolyMap (a ': as) = [a] :<->: PolyMap as
+
+infixr 6 :<->:
+
+deriving instance Show (PolyMap '[])
+deriving instance (Show a, Show (PolyMap as)) => Show (PolyMap (a ': as))
