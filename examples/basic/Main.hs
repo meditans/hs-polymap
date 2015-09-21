4544a2a34489d0af0abf78fe8535c1d844b028ee
Shockk <shokku.ra@gmail.com>
2015-09-22 00:50:18 +0100

    Update basic example

--- examples/basic/Main.hs
+++ examples/basic/Main.hs
@@ -1,5 +1,14 @@
-import Data.PolyMap 
+{-# LANGUAGE DataKinds #-}
 
-main = print polymap
+import Data.PolyMap (Relation((:<->:)))
+import qualified Data.PolyMap as PM
 
-polymap = ["asdf"] :<->: [5] :<->: EmptyPolyMap
+main = sequence $ map print [empty, singleton, two]
+
+empty :: PM.PolyMap '[String, Integer]
+empty = PM.empty
+
+singleton :: PM.PolyMap '[String, Integer]
+singleton = PM.singleton $ "one" :<->: 1 :<->: PM.UnitRelation
+
+two = PM.insert ("two" :<->: 2 :<->: PM.UnitRelation) singleton
