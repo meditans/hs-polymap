41bebf64a664d5844886529c35676e942a18b2be
Shockk <shokku.ra@gmail.com>
2015-09-23 04:23:26 +0100

    Change kind of PolyMap to:  [(*, *)] -> *

--- src/Data/PolyMap.hs
+++ src/Data/PolyMap.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE Safe #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
@@ -36,6 +37,7 @@ module Data.PolyMap
 
 import Data.PolyMap.Nat
 import Data.PolyMap.Relation
+import Data.PolyMap.Storage
 
 listElemAt :: Int -> [a] -> Maybe a
 listElemAt i xs
@@ -61,22 +63,26 @@ type family TypeAt (n :: Nat) (as :: [*]) where
     TypeAt 'Z (a ': as) = a
     TypeAt ('S n) (a ': as) = TypeAt n as
 
-data family PolyMap (as :: [*]) :: *
+type family MapFst (as :: [(*, *)]) :: [*] where
+    MapFst '[] = '[]
+    MapFst ('(a, b) ': as) = a ': MapFst as
+
+data family PolyMap (as :: [(*, *)]) :: *
 data instance PolyMap '[] = UnitPolyMap
-data instance PolyMap (a ': as) = [a] :<=>: PolyMap as
+data instance PolyMap ('(a, b) ': as) = [a] :<=>: PolyMap as
 
 infixr 4 :<=>:
 
 deriving instance Show (PolyMap '[])
-deriving instance (Show a, Show (PolyMap as)) => Show (PolyMap (a ': as))
+deriving instance (Show a, Show (PolyMap as)) => Show (PolyMap ('(a, b) ': as))
 
-class PolyMapClass (as :: [*]) where
+class PolyMapClass (as :: [(*, *)]) where
     null :: PolyMap as -> Bool
     size :: PolyMap as -> Int
     empty :: PolyMap as
-    singleton' :: Relation as -> PolyMap as
-    insert' :: Relation as -> PolyMap as -> PolyMap as
-    relationAt :: Int -> PolyMap as -> Maybe (Relation as)
+    singleton' :: Relation (MapFst as) -> PolyMap as
+    insert' :: Relation (MapFst as) -> PolyMap as -> PolyMap as
+    relationAt :: Int -> PolyMap as -> Maybe (Relation (MapFst as))
 
 instance PolyMapClass '[] where
     null UnitPolyMap = True
@@ -86,7 +92,7 @@ instance PolyMapClass '[] where
     insert' UnitRelation UnitPolyMap = UnitPolyMap
     relationAt _ UnitPolyMap = Just UnitRelation
 
-instance PolyMapClass as => PolyMapClass (a ': as) where
+instance PolyMapClass as => PolyMapClass ('(a, b) ': as) where
     null (xs :<=>: _) = Prelude.null xs
     size (xs :<=>: _) = length xs
     empty = [] :<=>: empty
@@ -94,32 +100,32 @@ instance PolyMapClass as => PolyMapClass (a ': as) where
     insert' (x :<->: xs) (m :<=>: ms) = m ++ [x]:<=>: insert' xs ms
     relationAt i (m :<=>: ms) = (:<->:) <$> listElemAt i m <*> relationAt i ms
 
-class PolyMapLookup (n :: Nat) (as :: [*]) where
-    member :: Proxy n -> TypeAt n as -> PolyMap as -> Bool
-    lookupIndex :: Proxy n -> TypeAt n as -> PolyMap as -> Maybe Int
+class PolyMapLookup (n :: Nat) (as :: [(*, *)]) where
+    member :: Proxy n -> TypeAt n (MapFst as) -> PolyMap as -> Bool
+    lookupIndex :: Proxy n -> TypeAt n (MapFst as) -> PolyMap as -> Maybe Int
 
 instance PolyMapLookup n '[] where
     member Proxy _ UnitPolyMap = False
     lookupIndex Proxy _ UnitPolyMap = Nothing
 
-instance Eq a => PolyMapLookup 'Z (a ': as) where
+instance Eq a => PolyMapLookup 'Z ('(a, b) ': as) where
     member Proxy x (xs :<=>: _) = elem x xs
     lookupIndex Proxy x (xs :<=>: _) = listLookupIndex x xs
 
-instance (PolyMapLookup n as) => PolyMapLookup ('S n) (a ': as) where
+instance (PolyMapLookup n as) => PolyMapLookup ('S n) ('(a, b) ': as) where
     member Proxy x (_ :<=>: ms) = member (Proxy :: Proxy n) x ms
     lookupIndex Proxy x (_ :<=>: ms) = lookupIndex (Proxy :: Proxy n) x ms
 
-notMember :: PolyMapLookup n as => Proxy n -> TypeAt n as -> PolyMap as -> Bool
+notMember :: PolyMapLookup n as => Proxy n -> TypeAt n (MapFst as) -> PolyMap as -> Bool
 notMember proxy x m = not (member proxy x m)
 
-lookup :: (PolyMapClass as, PolyMapLookup n as) => Proxy n -> TypeAt n as -> PolyMap as -> Maybe (Relation as)
+lookup :: (PolyMapClass as, PolyMapLookup n as) => Proxy n -> TypeAt n (MapFst as) -> PolyMap as -> Maybe (Relation (MapFst as))
 lookup proxy x m = case lookupIndex proxy x m of
     Nothing -> Nothing
     Just i  -> relationAt i m
 
-singleton :: (PolyMapClass as, ToRelation a as) => a -> PolyMap as
+singleton :: (PolyMapClass as, ToRelation a (MapFst as)) => a -> PolyMap as
 singleton r = singleton' (toRelation r)
 
-insert :: (PolyMapClass as, ToRelation a as) => a -> PolyMap as -> PolyMap as
+insert :: (PolyMapClass as, ToRelation a (MapFst as)) => a -> PolyMap as -> PolyMap as
 insert r m = insert' (toRelation r) m
---
+++ src/Data/PolyMap/Storage.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE Safe #-}
+
+{-|
+  Module      : Data.PolyMap.Storage
+  Copyright   : (c) 2015 David Farrell
+  License     : PublicDomain
+  Stability   : unstable
+  Portability : non-portable (GHC extensions)
+
+  Storage typeclass for polymaps.
+-}
+
+module Data.PolyMap.Storage where
+
+
